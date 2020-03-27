8089fbdccfbfcbfde8d8218728fb9ebd16e3f25c
Michael Ploughman <michaelhploughman@gmail.com>
2020-03-26 20:58:32 -0600

    Setup DB connections, Start recipes-endpoint-test

--- test/recipes-enpoint.spec.js
+++ test/recipes-enpoint.spec.js
@@ -2,13 +2,13 @@ const knex = require('knex')
 const app = require('../src/app')
 const helpers = require('./test-helpers')
 
-describe('Reviews Endpoints', function() {
+describe('Recipes Endpoints', function() {
   let db
 
   const {
-    testThings,
+    testRecipes,
     testUsers,
-  } = helpers.makeThingsFixtures()
+  } = helpers.makeRecipesFixtures()
 
   before('make knex instance', () => {
     db = knex({
@@ -23,3 +23,15 @@ describe('Reviews Endpoints', function() {
   before('cleanup', () => helpers.cleanTables(db))
 
   afterEach('cleanup', () => helpers.cleanTables(db))
+
+  describe(`GET /api/recipes`, () => {
+    context(`Given no recipes`, () => {
+      it(`responds with 200 and an empty list`, () => {
+        return supertest(app)
+          .get('/api/recipes')
+          .set('Authorization', helpers.makeAuthHeader(testUsers[0]))
+          .expect(200, [])
+      })
+    })
+
+}
