5547a4be59873f9094afbed04e1c295f3a568e14
Michael Ploughman <michaelhploughman@gmail.com>
2020-04-03 17:12:38 -0600

    Trying different cors configuration

--- src/app.js
+++ src/app.js
@@ -16,17 +16,17 @@ app.use(morgan(morganOption));
 app.use(helmet());
 const allowedOrigins = ['http://localhost:3000', 'https://only-tasteful.now.sh/']
 app.use(cors(
-  {
-    origin: function (origin, callback) {
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.indexOf(origin) === -1) {
-        const msg = 
-        'The CORS policy for this site does not allow access from the specified Origin.';
-        return callback(new Error(msg), false);
-      }
-      return callback(null, true);
-    }
-  }
+  // {
+  //   origin: function (origin, callback) {
+  //     if (!origin) return callback(null, true);
+  //     if (allowedOrigins.indexOf(origin) === -1) {
+  //       const msg = 
+  //       'The CORS policy for this site does not allow access from the specified Origin.';
+  //       return callback(new Error(msg), false);
+  //     }
+  //     return callback(null, true);
+  //   }
+  // }
 ));
 
 // ROUTERS
